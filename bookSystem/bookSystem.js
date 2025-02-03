ebfa411e93048cae34bc1e4f101caa69cb2cc308
Roualadhari <roua.ladhari@horizon-tech.tn>
2025-02-03 12:57:15 -0500

    message

--- bookSystem/bookSystem.js
+++ bookSystem/bookSystem.js
@@ -18,15 +18,15 @@ let books = [];
             } else {
                 alert('Please fill in all fields correctly.');
             }
-        }
+}
 
-        function showbooks() {
-        const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
-        <p><strong>Book Name: </strong>${book.name}</p>
-        <p><strong>Author Name:</strong> ${book.authorName}</p>
-        <p><strong>Book Description:</strong> ${book.bookDescription}</p>
-        <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
-        `<button onclick=deleteBook(${index})>Delete</button>`
+function showbooks() {
+    const booksDiv = books.map((book, index) => `<h1>book Number: ${index + 1}</h1>
+    <p><strong>Book Name: </strong>${book.name}</p>
+    <p><strong>Author Name:</strong> ${book.authorName}</p>
+    <p><strong>Book Description:</strong> ${book.bookDescription}</p>
+    <p><strong>No. of Pages:</strong> ${book.pagesNumber} page(s)</p>`
+    `<button onclick=deleteBook(${index})>Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
 }
