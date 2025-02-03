1a5f1f8f48a7c372a11940f56c041967f7530af0
Roualadhari <roua.ladhari@horizon-tech.tn>
2025-02-03 12:31:20 -0500

    message

---
+++ restaurantMenu/restaurantMenu.js
@@ -0,0 +1,16 @@
+const breakfastMenu = ['Pancakes - 12$', 'Eggs Benedict - 15.5$', 'Oatmeal - 10.23$', 'Frittata - 13.56$'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
